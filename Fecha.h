aa29e913c4fd3117e0203aa07364692d6344cc3c
Mati Nicotra <matiasanicotra@gmail.com>
2023-06-13 19:26:11 -0300

    comentarios y agrego clase fecha sin empezar

---
+++ Fecha.h
@@ -0,0 +1,16 @@
+#ifndef FECHA_H
+#define FECHA_H
+
+
+class Fecha
+{
+	public:
+		Fecha();
+		virtual ~Fecha();
+
+	protected:
+
+	private:
+};
+
+#endif // FECHA_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
 
+/*
+clases
+fecha
+
+AUTO
+char patente
+nombre
+marca
+modelo anio
+estado
+
+
+PERSONA
+dni
+nombre
+apellido
+email
+telefono
+direccion
+estado
+
+PERSONA empleado
+cargo
+sueldo
+fecha alta
+
+PERSONA cliente
+fecha alta
+
+PERSONA proveedor
+comentarios
+rubro
+costo
+
+AUTOCLIENTE
+dni cliente o id
+patente auto
+estado
+
+TRABAJO
+patente
+problema string
+presupuesto
+estado de trabajo
+
+
+
+
+*/
+
 using namespace std;
 
 int main()
