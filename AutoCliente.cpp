26cd782be61b598ce95740782c45a7925a091ab5
Mati Nicotra <matiasanicotra@gmail.com>
2023-06-15 19:06:56 -0300

    setters y getters de clase autocliente

--- AutoCliente.cpp
+++ AutoCliente.cpp
@@ -3,9 +3,35 @@
 AutoCliente::AutoCliente() {
 	_idCliente = 0;
 	_idVehiculo = 0;
+	_estado = true;
 }
 
 AutoCliente::AutoCliente(int idCliente, int idVehiculo) {
 	_idCliente = idCliente;
 	_idVehiculo = idVehiculo;
+	_estado = true;
+}
+
+void AutoCliente::setIdCliente(int idCliente) {
+	_idCliente = idCliente;
+}
+
+void AutoCliente::setIdVehiculo(int idVehiculo) {
+	_idVehiculo = idVehiculo;
+}
+
+void AutoCliente::setEstado(bool estado) {
+	_estado = estado;
+}
+
+int AutoCliente::getIdCliente() {
+	return _idCliente;
+}
+
+int AutoCliente::getIdVehiculo() {
+	return _idVehiculo;
+}
+
+bool AutoCliente::getEstado() {
+	return _estado;
 }
--- AutoCliente.h
+++ AutoCliente.h
@@ -7,12 +7,19 @@ class AutoCliente
 private:
 	int _idCliente;
 	int _idVehiculo;
+	bool _estado;
 
 public:
 	AutoCliente();
 	AutoCliente(int idCliente, int idVehiculo);
 
+	void setIdCliente(int idCliente);
+	void setIdVehiculo(int idVehiculo);
+	void setEstado(bool estado);
 
+	int getIdCliente();
+	int getIdVehiculo();
+	bool getEstado();
 };
 
 #endif // AUTOCLIENTE_H
