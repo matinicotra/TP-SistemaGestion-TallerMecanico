1e4446f3bff3d550362293bd07a7bbcdb5b76f17
Mati Nicotra <matiasanicotra@gmail.com>
2023-06-15 19:00:40 -0300

    agrego clase autoCliente

---
+++ AutoCliente.cpp
@@ -0,0 +1,11 @@
+#include "AutoCliente.h"
+
+AutoCliente::AutoCliente() {
+	_idCliente = 0;
+	_idVehiculo = 0;
+}
+
+AutoCliente::AutoCliente(int idCliente, int idVehiculo) {
+	_idCliente = idCliente;
+	_idVehiculo = idVehiculo;
+}
---
+++ AutoCliente.h
@@ -0,0 +1,18 @@
+#ifndef AUTOCLIENTE_H
+#define AUTOCLIENTE_H
+
+
+class AutoCliente
+{
+private:
+	int _idCliente;
+	int _idVehiculo;
+
+public:
+	AutoCliente();
+	AutoCliente(int idCliente, int idVehiculo);
+
+
+};
+
+#endif // AUTOCLIENTE_H
