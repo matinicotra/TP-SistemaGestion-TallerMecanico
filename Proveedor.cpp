6c3ad297ad2842d6a75d40be90430fc16515f81a
Mati Nicotra <matiasanicotra@gmail.com>
2023-06-16 21:07:47 -0300

    clase proveedor
    agrego propiedades a clase proveedor

--- Proveedor.cpp
+++ Proveedor.cpp
@@ -11,6 +11,22 @@ void Proveedor::setRubro(std::string rubro) {
 	_rubro = rubro;
 }
 
+void Proveedor::setAutoparte(std::string autoparte) {
+	_autoparte = autoparte;
+}
+
+void Proveedor::setOrigeFabricacion(std::string origenFabricacion) {
+	_origenFabricacion = origenFabricacion;
+}
+
 std::string Proveedor::getRubro() {
 	return _rubro;
+}
+
+std::string Proveedor::getAutoparte() {
+	return _autoparte;
+}
+
+std::string Proveedor::getOrigenFabricacion() {
+	return _origenFabricacion;
 }
--- Proveedor.h
+++ Proveedor.h
@@ -7,14 +7,20 @@ class Proveedor: public Persona {
 private:
 	int _idProveedor;
 	std::string _rubro;
+	std::string _autoparte;
+	std::string _origenFabricacion;
 	bool _estado;
 
 public:
 	Proveedor();
 
 	void setRubro(std::string rubro);
+	void setAutoparte(std::string autoparte);
+	void setOrigeFabricacion(std::string origenFabricacion);
 
 	std::string getRubro();
+	std::string getAutoparte();
+	std::string getOrigenFabricacion();
 
 };
 
