c6ffea4f1d9bbdf28bb44ece7adb994c39cfc18f
Brian Sablich <sablichbrian@yahoo.com.ar>
2023-06-19 20:47:58 -0300

    ProveedorArchivo

---
+++ ProveedorArchivo.cpp
@@ -0,0 +1,19 @@
+///Ejercicio:
+///Autor:
+///Fecha:
+///Comentario:
+
+# include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+	return 0;
+	   }#include "ProveedorArchivo.h"
+
+ProveedorArchivo::ProveedorArchivo()
+{
+    //ctor
+}
---
+++ ProveedorArchivo.h
@@ -0,0 +1,15 @@
+#ifndef PROVEEDORARCHIVO_H
+#define PROVEEDORARCHIVO_H
+
+
+class ProveedorArchivo
+{
+    public:
+        ProveedorArchivo();
+
+    protected:
+
+    private:
+};
+
+#endif // PROVEEDORARCHIVO_H
