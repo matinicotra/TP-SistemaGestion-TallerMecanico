9c911d09544917ec3af4b3deae575c302c55b4fa
Brian Sablich <sablichbrian@yahoo.com.ar>
2023-06-20 14:43:40 -0300

    Menus y gitignore

---
+++ menu.cpp
@@ -0,0 +1,18 @@
+#include "Menu.h"
+
+void Menu::displayMain(){
+    while (true){
+            system("cls");
+        std::cout<<"================================"<<endl;
+        std::cout<<"============= MENU ============="<<endl;
+        std::cout<<"================================"<<endl<<endl;
+        std::cout<<"-1 Realizar cargas en el sistema."<<endl;
+        std::cout<<"-2 Consultas."<<endl;
+        std::cout<<"-3 Listados."<<endl;
+        std::cout<<"-3 Informes."<<endl;
+        std::cout<<"-4 Manejo de archivos."<<endl;
+        std::cout<<"-0 Salir."<<endl;
+    }
+}
+
+void Menu
---
+++ menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+class Menu
+{
+    public:
+        void displayMain();
+        void menuCargas();
+
+
+};
+
+#endif // MENU_H
