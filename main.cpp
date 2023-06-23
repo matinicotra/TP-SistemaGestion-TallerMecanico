0e94e759394f58ae4bdd4326b195206b0f885a97
Mati Nicotra <matiasanicotra@gmail.com>
2023-06-23 10:45:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 using namespace std;
 
-#include "TrabajoManager.h"
-#include "Trabajo.h"
 int main()
 {
-	TrabajoManager aux;
-//	Trabajo reg(1, 1, "abc123", "31835788", "1324253", "3425343", 2);
-	Trabajo reg;
-	aux.Listar(reg);
 
     return 0;
 }
